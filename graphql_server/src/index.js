c36eb6283214aee1ed05f99ced8428d62638d96d
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-02 04:16:02 +0545

    [graphql_server]: enabling cors for client connection and refactoring codes

--- graphql_server/src/index.js
+++ graphql_server/src/index.js
@@ -1,34 +1,56 @@
 require('dotenv').config();
 
 import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
-import http from 'http';
 
 import { resolvers, typeDefs } from './graphql';
 import connectDB from './db/connectDB';
 import User from './models/User';
 
+const auth = require('./middlewares/auth');
+
+const app = express();
+connectDB();
+
+app.use(auth);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const { isAuth, user } = req;
+    return {
+      User,
+      req,
+      isAuth,
+      user,
+    };
+  },
+});
+
 async function mountServer() {
-  const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => {
-      return {
-        User,
-      };
-    },
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-  await server.start();
-  connectDB();
-  server.applyMiddleware({ app });
-  const port = process.env.PORT;
-  await new Promise((resolve) => httpServer.listen({ port }, resolve));
-  console.log(
-    `server listening on port http://localhost:${port}${server.graphqlPath}`,
-  );
+  try {
+    await server.start();
+    server.applyMiddleware({
+      app,
+    });
+    const port = process.env.PORT;
+    app.listen(port, () => {
+      console.log(
+        `server listening on port http://localhost:${port}${server.graphqlPath}`,
+      );
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 mountServer();
