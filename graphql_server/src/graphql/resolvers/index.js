312f8439680e903b6e7169e7ddd2f6ee2c2b4a1c
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-01 20:50:04 +0545

    [graphql_server]: login and registration logic implemented hashing password

--- graphql_server/src/graphql/resolvers/index.js
+++ graphql_server/src/graphql/resolvers/index.js
@@ -1,6 +1,44 @@
+import bcrypt from 'bcryptjs';
+
 const resolvers = {
   Query: {
     hello: () => 'Hello world',
+  },
+  Mutation: {
+    register: async (
+      parent,
+      { name, email, password, avatar },
+      { User },
+      info,
+    ) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await new User({
+          name,
+          email,
+          password: hashedPassword,
+          avatar,
+        });
+        const user = await newUser.save();
+        return user;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+    login: async (parent, { email, password }, { User }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('Email not found. Please register.');
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+          throw new Error(
+            'Either email or password is incorrect. Please verify',
+          );
+        return user;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
--- graphql_server/src/graphql/typeDefs/index.js
+++ graphql_server/src/graphql/typeDefs/index.js
@@ -1,8 +1,25 @@
 import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    avatar: String!
+  }
+
   type Query {
     hello: String
+  }
+  type Mutation {
+    register(
+      name: String!
+      email: String!
+      password: String!
+      avatar: String
+    ): User
+    login(email: String!, password: String!): User
   }
 `;
 
