64af615b37105da0c8f15ca491f51f9a5fc06e2d
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-02 13:43:32 +0545

    [client]: made some routes and fetch user from backend

---
+++ client/src/Routing.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Loading from './components/Loading';
+import Error from './components/Error';
+
+const getProfile = gql`
+  query {
+    getProfile {
+      id
+      name
+      email
+      avatar
+    }
+  }
+`;
+
+const Routing = () => {
+  const { data, error, loading } = useQuery(getProfile);
+
+  if (error) return <Error />;
+  if (loading) return <Loading />;
+
+  console.log(data.getProfile);
+
+  const { name, email } = data.getProfile;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home name={name} email={email} />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
