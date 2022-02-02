fa351f408b587f4592b4f39c49216284492aaf3a
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-02 15:41:11 +0545

    [graphql_server]: separating logic of different entity to different files

---
+++ graphql_server/src/graphql/resolvers/post.js
@@ -0,0 +1,101 @@
+export default {
+  Query: {
+    getCategory: async (parent, args, { Category }) => {
+      try {
+        const categories = Category.find({});
+        return categories;
+      } catch (err) {
+        throw new Error();
+      }
+    },
+    getPost: async (parent, args, { Post }, info) => {
+      try {
+        const post = await Post.find().populate('user');
+        return post;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+    getPostById: async (parent, { id }, { Post }, info) => {
+      try {
+        const post = await Post.findById(id);
+        if (!post) {
+          throw new Error(err.message);
+        }
+        return post;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+    getUserPost: async (parent, args, { Post, user }, info) => {
+      try {
+        const post = await Post.findOne({ user: user._id }).populate('user');
+        return post;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+  },
+  Mutation: {
+    addCategory: async (parent, { name }, { Category }) => {
+      try {
+        const category = await new Category({ name });
+        await category.save();
+        console.log(category);
+        return category;
+      } catch (err) {
+        console.log(err);
+        throw new Error(err);
+      }
+    },
+    createPost: async (parent, { postInput }, { Post, user }, info) => {
+      try {
+        const newPost = await new Post({
+          ...postInput,
+          user: user._id.toString(),
+        });
+        const post = await newPost.save();
+        return post;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+    editPost: async (parent, { id, postInput }, { Post, user }, info) => {
+      try {
+        let post = await Post.findById(id).populate('user');
+        if (!post) {
+          throw new Error('post with that id in not found');
+        }
+
+        console.log(post);
+        if (post.user.email !== user.email) {
+          throw new Error('you cannot edit other post');
+        }
+
+        post = await Post.findByIdAndUpdate(
+          id,
+          { ...postInput },
+          { new: true },
+        );
+        return post;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    deletePost: async (parent, { id }, { Post, user }, info) => {
+      try {
+        const post = await Post.findById(id).populate('user');
+        if (!post) {
+          throw new Error('Post with that ID is not found');
+        }
+        if (post.user.email !== user.email) {
+          throw new Error('you cannot delete other post');
+        }
+        await Post.findByIdAndDelete(id);
+        return 'Post deleted succeessfully';
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+  },
+};
---
+++ graphql_server/src/graphql/resolvers/user.js
@@ -0,0 +1,64 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const createToken = (user, secret) => {
+  const { email, name } = user;
+  return jwt.sign({ email, name }, secret, {
+    expiresIn: '5d',
+  });
+};
+
+export default {
+  Query: {
+    getProfile: async (parent, args, { req, user }) => {
+      return user;
+    },
+  },
+  Mutation: {
+    register: async (
+      parent,
+      { name, email, password, avatar },
+      { User },
+      info,
+    ) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await new User({
+          name,
+          email,
+          password: hashedPassword,
+          avatar,
+        });
+        const user = await newUser.save();
+        const payload = {
+          name: user.name,
+          email: user.email,
+        };
+        const token = createToken(payload, process.env.JWT_SECRET);
+        // return 'Registered successfully';
+        return token;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+    login: async (parent, { email, password }, { User }) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('Email not found. Please register.');
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+          throw new Error(
+            'Either email or password is incorrect. Please verify',
+          );
+        const payload = {
+          name: user.name,
+          email: user.email,
+        };
+        const token = createToken(payload, process.env.JWT_SECRET);
+        return token;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
