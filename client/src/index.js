317813d974f4d0b94a227597614a9ed693f5f42f
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-02 13:44:59 +0545

    [client]: imported main Route and also imported and implemented apollo client for state management

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {
+  InMemoryCache,
+  ApolloClient,
+  ApolloProvider,
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-};
+import Routing from './Routing';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8848/graphql',
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('auth_token');
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const root = document.querySelector('#root');
 render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Routing />
+    </ApolloProvider>
   </React.StrictMode>,
   root,
 );
