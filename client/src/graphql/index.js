2409ccef5925afef341c4c4ed7f6d3dc1d51c47c
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-03 23:55:06 +0545

    [client]: variables to interact to backend and things that need to be displayed in frontned

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const GET_PROFILE = gql`
   query {
     getProfile {
-      id
+      _id
       username
       avatar
     }
@@ -21,5 +21,44 @@ export const LOGIN_USER = gql`
 export const REGISTER_USER = gql`
   mutation ($username: String!, $password: String!) {
     register(username: $username, password: $password)
+  }
+`;
+
+export const GET_POSTS = gql`
+  query {
+    getPost {
+      _id
+      title
+      photo
+      description
+      user {
+        username
+      }
+      categories
+    }
+  }
+`;
+
+export const CREATE_POST = gql`
+  mutation CreatePost($postInput: PostInput) {
+    createPost(postInput: $postInput)
+  }
+`;
+
+export const EDIT_POST = gql`
+  mutation EditPost($editPostId: ID, $editPostPostInput2: PostInput) {
+    editPost(id: $editPostId, postInput: $editPostPostInput2) {
+      _id
+      title
+      description
+      categories
+      photo
+    }
+  }
+`;
+
+export const DELETE_POST = gql`
+  mutation DeletePost($deletePostId: ID!) {
+    deletePost(id: $deletePostId)
   }
 `;
