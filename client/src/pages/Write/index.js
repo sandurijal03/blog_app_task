416c4de25433a99937bc2c6a54ca991a976b4c43
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-03 23:53:58 +0545

    [client]: creating post and sending data to database

--- client/src/pages/Write/index.js
+++ client/src/pages/Write/index.js
@@ -1,13 +1,102 @@
-import React from 'react';
-import withAuth from '../../components/withAuth';
-import withSession from '../../components/withSession';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { toast } from 'react-toastify';
+
+// import withAuth from '../../components/withAuth';
+// import withSession from '../../components/withSession';
+import { CREATE_POST } from '../../graphql';
+
+import {
+  Container,
+  Form,
+  Input,
+  InputContainer,
+  Submit,
+  Textarea,
+  WriteIcon,
+} from './style';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const Write = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState('');
+  const [category, setCategory] = useState('');
+  const categories = category.split(',');
+
+  const [createPost, { loading, error }] = useMutation(CREATE_POST, {
+    variables: {
+      postInput: {
+        title,
+        description,
+        photo: file,
+        categories,
+      },
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createPost().then((data) => {
+      toast(data.data.createPost);
+      setTitle('');
+      setDescription('');
+      setFile('');
+      setCategory('');
+    });
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
   return (
-    <div>
-      <h1>Write</h1>
-    </div>
+    <Container>
+      <Form>
+        <InputContainer>
+          <label htmlFor='fileInput'>
+            <WriteIcon />
+          </label>
+          {/* <Input
+            type='file'
+            id='fileInput'
+            style={{ display: 'none' }}
+            value={file}
+            onChange={(e) => setFile(e.target.files[0])}
+          /> */}
+
+          <Input
+            type='text'
+            className='form-control'
+            id='title'
+            placeholder='Enter Post title here'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Input
+            type='text'
+            value={category}
+            name='categories'
+            placeholder='categories1, categories2'
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <Textarea
+            className='form-control'
+            id='body'
+            rows='3'
+            placeholder='Enter Blog Description here'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></Textarea>
+        </InputContainer>
+        <Submit onClick={handleSubmit}>Publish</Submit>
+      </Form>
+    </Container>
   );
 };
 
-export default withAuth(Write);
+export default Write;
---
+++ client/src/pages/Write/style.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import { FaPlus } from 'react-icons/fa';
+
+const Container = styled.div`
+  padding-top: 50px;
+`;
+
+const WriteIcon = styled(FaPlus)`
+  width: 25px;
+  height: 25px;
+  font-size: 20px;
+  border: 1px solid;
+  border-radius: 50%;
+  color: rgb(129, 125, 125);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const Form = styled.form`
+  position: relative;
+`;
+
+const InputContainer = styled.div`
+  margin-left: 150px;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  font-size: 30px;
+  border: none;
+  padding: 20px;
+  width: 70vw;
+  :focus {
+    outline-style: none;
+  }
+  ::placeholder {
+    color: rgb(189, 185, 185);
+    font-weight: 400;
+  }
+`;
+
+const Textarea = styled.textarea`
+  font-size: 30px;
+  border: none;
+  padding: 20px;
+  width: 70vw;
+  height: 100vh;
+  font-family: inherit;
+  :focus {
+    outline-style: none;
+  }
+  ::placeholder {
+    color: rgb(189, 185, 185);
+    font-weight: 400;
+  }
+`;
+
+const Submit = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 50px;
+  color: white;
+  background-color: teal;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
+  font-size: 16px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+// exporting variables
+export { Container, WriteIcon, Form, InputContainer, Input, Textarea, Submit };
