df4d5085c64f3e6850ee65f9232554e7b7fb365b
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-07 03:50:12 +0545

    [client]: withAuth higher order function restricting unregistered user to create new blog

--- client/src/components/withAuth.js
+++ client/src/components/withAuth.js
@@ -2,15 +2,29 @@ import { useQuery } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import Loading from './Loading';
 import { GET_PROFILE } from '../graphql/index';
+import { useEffect } from 'react';
+
+const Redirect = ({ getProfile }) => {
+  const navigate = useNavigate('/');
+
+  useEffect(() => {
+    if (getProfile === null) {
+      navigate('/');
+    }
+  });
+  return null;
+};
 
 const withAuth = (conditionalFunc) => (Component) => (props) => {
   const { data, loading } = useQuery(GET_PROFILE);
-  const navigate = useNavigate();
   if (loading) return <Loading />;
+
+  const getProfile = data.getProfile;
+
   return conditionalFunc(data) ? (
     <Component {...props} />
   ) : (
-    navigate('/', { replace: true })
+    <Redirect getProfile={getProfile} />
   );
 };
 
