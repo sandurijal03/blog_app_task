3565515241c651bf8d8a2e9b7bf616bc97a56e65
Sandip Rijal <sandurijal03@hotmail.com>
2022-02-07 02:26:48 +0545

    [client]: hero component

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: url('/assets/home.jpg');
+  height: 400px;
+  background-position: center;
+  background-size: cover;
+  margin: 80px 0;
+`;
+
+const Hero = () => {
+  return <Container></Container>;
+};
+
+export default Hero;
